4a12c07a8a0b8ae89a880ad5180b079f7cbcd566
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-09 13:59:24 -0700

    total average rating and total cost of trips for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,15 @@
 class Driver < ApplicationRecord
   has_many :trips
-
-  # TODO add total rides
-  # TODO add average rating
+  
+  def average_rating
+    all_ratings = self.trips.map { |trip| trip.rating }
+    sum = all_ratings.sum
+    return sum / all_ratings.length.to_f
+  end 
+  
+  def total_rides
+    total_cost = 0 
+    self.trips.each { |trip| total_cost += trip.cost }
+    return total_cost
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,12 @@
 class Passenger < ApplicationRecord
   has_many :trips
-
+  
   # TODO Need to add total cost of trips
+  def self.total_cost 
+    total_cost = 0 
+    @passenger.trips do |trip|
+      total_cost += trip.cost 
+    end 
+    return total_cost
+  end 
 end
