b2237a7f86b44f539e7692a89776720a46fd6b2d
thenora <thenora@gmail.com>
2020-05-10 20:44:20 -0700

    trips validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,9 @@ class Trip < ApplicationRecord
   belongs_to :passenger 
   belongs_to :driver
 
+  validates :date, presence: true # requires every trip must have a date
+  validates :cost, presence: true # requires every trip must have a cost
+  
   def self.find_driver 
     @drivers = Driver.all 
     
