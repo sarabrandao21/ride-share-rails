4df97fa5427d712287241a010563d8919cae5b58
thenora <thenora@gmail.com>
2020-05-10 19:09:03 -0700

    added missing find_driver method to trip - oops

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,15 @@
 class Trip < ApplicationRecord
   belongs_to :passenger 
   belongs_to :driver
+
+  def self.find_driver 
+    @drivers = Driver.all 
+    
+    @drivers.each do |driver|
+      if driver.status == "available"
+        return driver
+      end
+    end
+  end
+
 end
