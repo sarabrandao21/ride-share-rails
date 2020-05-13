c5526817ace5ab9621132b87f68798772542181e
thenora <thenora@gmail.com>
2020-05-12 17:57:36 -0700

    Final final clean up

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,9 +3,8 @@ class Driver < ApplicationRecord
   
   validates :name, presence: true # requires every driver must have a name
   # note: vins actually have a fancy check sum validation, a basic regex would only catch the most obvious fake VINs - would need to make a more complex method to truly validate the vin
-  validates :vin, presence: true#, format: /[A-HJ-NPR-Z0-9]{17}/ 
-  # TODO - add vin verification back in, hiding for ease of creating new records during build
-  # requires vin must have a value, and that it must be a combination of 17 numbers and certain capital letters.
+  validates :vin, presence: true
+  # format: /[A-HJ-NPR-Z0-9]{17}/  would require vin must have a value, and that it must be a combination of 17 numbers and certain capital letters.
   
   
   def average_rating
